3f27628e8625ef5cb8cb312f661a1cab450bed21
GeunHwa Lee <burzum17@naver.com>
2019-03-13 23:55:56 +0900

    modifying RxSwift MVVM

--- SwiftyDesignPattern/ViewModel/RepositoryViewModel.swift
+++ SwiftyDesignPattern/ViewModel/RepositoryViewModel.swift
@@ -11,8 +11,16 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class RepositoryViewModel {
-//    var viewWillAppear:PublishBehavior<T>
+protocol ViewModelType{
+    associatedtype Input
+    associatedtype Output
+    
+    func transform(_ input:Input) -> Output
+    
+}
+
+class RepositoryViewModel:ViewModelType{
+    
     var gitHubTitle: String
     var starCount: Int = 0
     var description: String?
@@ -23,9 +31,23 @@ class RepositoryViewModel {
         self.description = model.description
         self.url = URL(string: model.url)!
     }
+    
+    func transform(_ input: Input) -> Output {
+//        let reloadData = input.refreshTap
+        return RepositoryViewModel.Output
+    }
 }
 
 extension RepositoryViewModel {
+    
+    struct Input{
+        let refreshTap:Driver<Void>
+    }
+    
+    struct Output{
+        let reloadData:Driver<Void>
+    }
+    
     func configure(_ view: UITableViewCell) {
         view.textLabel?.numberOfLines = 0
         view.detailTextLabel?.numberOfLines = 0
